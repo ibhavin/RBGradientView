ec41b199b1d98146b6b34378c9b171af1b911ec0
Bhavin Ramani <bhavin.ramani050@gmail.com>
2016-01-04 12:33:19 +0530

    RBGradientView
    RBGradientView is custom class of UIView.
    It will let you add gradient to your view with two layer or three layer.

---
+++ RBGradientView/RBTwoLayerGradientView.m
@@ -0,0 +1,86 @@
+//
+//  RBTwoLayerGradientView.m
+//  RBGradientView
+//
+//  Created by My Mac on 04/01/16.
+//  Copyright (c) 2016 My Mac. All rights reserved.
+//
+
+#import "RBTwoLayerGradientView.h"
+
+@interface RBTwoLayerGradientView ()
+
+@property (nonatomic) IBInspectable UIColor *startColor;
+@property (nonatomic) IBInspectable UIColor *endColor;
+
+@end
+
+@implementation RBTwoLayerGradientView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        self.startColor     = [UIColor redColor];
+        self.endColor       = [UIColor blueColor];
+        self.borderWidth    = 2;
+        self.cornerRadious  = 10;
+        self.isHorizontal   = NO;
+        
+        [self customInit];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self customInit];
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    [self customInit];
+}
+
+- (void)setNeedsLayout {
+    [super setNeedsLayout];
+    [self setNeedsDisplay];
+}
+
+
+- (void)prepareForInterfaceBuilder {
+    
+    [self customInit];
+}
+
+- (void)customInit {
+    
+    
+    self.layer.cornerRadius = self.cornerRadious;
+    self.layer.borderWidth = self.borderWidth;
+    
+    if (self.cornerRadious > 0) {
+        self.layer.masksToBounds = YES;
+    }
+    
+    
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.bounds;
+    
+    gradient.colors = [NSArray arrayWithObjects:(id)[self.startColor CGColor],(id)[self.endColor CGColor], nil];
+    gradient.endPoint = (self.isHorizontal) ? CGPointMake(1, 0) : CGPointMake(0, 1);
+    [self.layer insertSublayer:gradient atIndex:0];
+    
+}
+
+@end
---
+++ RBGradientView/RBView.h
@@ -0,0 +1,18 @@
+//
+//  RBView.h
+//  RBGradientView
+//
+//  Created by My Mac on 04/01/16.
+//  Copyright (c) 2016 My Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface RBView : UIView
+
+@property (nonatomic) IBInspectable NSInteger borderWidth;
+@property (nonatomic) IBInspectable CGFloat cornerRadious;
+@property (nonatomic) IBInspectable BOOL isHorizontal;
+
+@end
